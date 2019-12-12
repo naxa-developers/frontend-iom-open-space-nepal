5142d08e1c3eb507665c872acf7a971906435581
swikritii <58209841+swikritii@users.noreply.github.com>
2019-12-12 15:26:24 +0545

    font-sizes maintained based on screen sizes for inner-map section

--- src/components/OpenSpace/Openspace.js
+++ src/components/OpenSpace/Openspace.js
@@ -47,10 +47,10 @@ class OpenSpace extends Component {
             <div className="container-fluid">
               <div className ="map-wrapper">
                 <div className="row no-gutters">
-                  <div className="col-md-8">
+                  <div className="col-md-7 col-lg-8">
                   <OpenspaceMap setProvince={this.setProvince} setcurrentLocation={this.setcurrentlocation} currentLocation={this.state.currentLocation} mapRefss={this.mapRefs} />
                   </div>
-                  <div className="col-md-4">
+                  <div className="col-md-5 col-lg-4">
                   <Sidebar mapRefs={this.mapRefs}  currentLocation={this.state.currentLocation} />
                   </div>
                 </div>
