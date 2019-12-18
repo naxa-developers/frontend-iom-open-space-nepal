c17b52e44023bc9a273e7f4392061d5f7926a99f
Siddharth009 <spaudel206@gmail.com>
2019-12-18 10:54:45 +0545

    issues on ide steps

--- src/components/Home/Identification.js
+++ src/components/Home/Identification.js
@@ -24,7 +24,7 @@ class Identification extends Component {
             this.setState({ steps: steps });
             
         })
-        axios.get(`http://iomapi.naxa.com.np/api/v1/open_space_ide/`)
+        axios.get(`https://iomapi.naxa.com.np/api/v1/open_space_ide/`)
         .then( res => {
             const data = res.data;
             this.setState({ data: data });
