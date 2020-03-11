8ac1263e95b42200c1cbc49a80f60aa536b1b591
siddharth009 <spaudel206@gmail.com>
2020-03-11 15:22:09 +0545

    loader changed on map

---
+++ src/components/OpenSpace/OpenSpaceDetails/MapLoader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+export default class MapLoader extends Component {
+    render() {
+        return (
+            <div>
+               
+            <div class="loaderMap" style={{display:'inline-block', marginRight:'8px', marginTop:'2px'}}></div><span >Loading</span> 
+            </div>
+        )
+    }
+}
+
