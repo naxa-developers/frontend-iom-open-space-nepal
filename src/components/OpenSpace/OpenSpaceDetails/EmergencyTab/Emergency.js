7bfb2f6d0627b69c908851316fcba5c7466ff812
siddharth009 <spaudel206@gmail.com>
2020-03-18 15:31:12 +0545

    handling empty messages on emergency tab

--- src/components/OpenSpace/OpenSpaceDetails/EmergencyTab/Emergency.js
+++ src/components/OpenSpace/OpenSpaceDetails/EmergencyTab/Emergency.js
@@ -1,60 +1,63 @@
-import React, {useState, useEffect} from 'react'
+import React, { Component} from 'react'
 import {Card} from 'react-bootstrap'
 import '../Gallery/Photo.css'
 import Axios from 'axios'
-function Emergency() {
+class Emergency extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             erData: null
+        }
+    }
+    
 
 
-    const[erData, seterData] = useState();
-useEffect(() => {
+componentDidMount() {
     let OID = localStorage.getItem("OpenspaceID")
     
     Axios.get(`http://139.59.67.104:8011/api/v1/message/?id=${OID}`).then(
         res => {
            
-            
-seterData(res.data)
+        this.setState({
+            erData: res.data
+        })
+
 
         }
-    )
+    ) 
+}
  
-    
-}, []) 
+render() {
+    this.state.erData&& console.log("l", Object.keys(this.state.erData).length);
 
+    return (
+        <div>
+            {
+                this.state.erData&& Object.keys(this.state.erData).length!==0 ?  this.state.erData.map((e) => {
+                    return(
+                        <Card>
+                                 <Card.Header>
+                                     <div style={{display:'flex'}}>
+                                      <div>
+                                       <h4 className="emerge-header">Agency Name:</h4>
+                                        </div>
+                                      <div> <span className="agency-name">{e.agency_name}</span>
+                                        </div> 
+                                      </div>
+                                     <div className="message">
+                               <h6 style={{color:'#174BDD'}}>Message:</h6> <span>{e.message} </span>
+                                </div>
+                                    </Card.Header>
+                        
+                        </Card>
+                    )
+                }) : <h6 style={{color: '#6D6E71', marginTop:'10px', fontSize:'1rem'}}>No messages available for this openspace</h6>
+            }
+        </div>
+    )
+}
+}
+   
 
-        return (
-            <div>
-                {
-                   erData&& erData.map((e) => {
-                        return(
-
-                <Card>
-                    <Card.Header>
-                        <div style={{display:'flex'}}>
-                        <div>
-                        <h4 className="emerge-header">Agency Name:</h4>
-                        </div>
-                        <div> <span className="agency-name">{e.agency_name}</span>
-                        </div> 
-                        </div>
-                        <div className="message">
-                 <h6 style={{color:'#174BDD'}}>Message:</h6> <span>{e.message} </span>
-                 </div>
-                    </Card.Header>
-  
-</Card>
-                        )
-                    })
-                }
-{/* <Card>
-                    <Card.Header> <h4>Agency Name:</h4> <span>Other agency</span>
-                    <Card.Body><h6>Message:</h6> <span>This is a long message</span></Card.Body>
-                    </Card.Header>
-  
-</Card> */}
-            </div>
-        )
-    }
-
-
-export default  Emergency;
+export default Emergency;
