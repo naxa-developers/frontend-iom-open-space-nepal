65b2f838f9eb97da96918a1120b5309a1ddda92a
susant <susant.naxa@gmail.com>
2022-08-26 12:38:46 +0545

    fix: resolve spacing problem

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,104 +1,119 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Axios from 'axios';
-
+import Axios from "axios";
 
 class Banner extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            texts: '',
-            loader: false
+    this.state = {
+      texts: "",
+      loader: false,
+    };
+  }
+  getData() {
+    Axios.get(`${process.env.BASE_URL_API}/header/`).then((res) => {
+      const text = res.data;
 
+      this.setState({
+        texts: text,
+        loader: true,
+      });
+    });
+  }
+  componentDidMount() {
+    this.getData();
+  }
 
-        }
-    }
-    getData() {
-        Axios.get(`${process.env.BASE_URL_API}/header/`)
-            .then(res => {
-                const text = res.data;
+  render() {
+    return (
+      <div className="banner-content">
+        <div className={this.props.language == "0" ? "intro" : "intro-nepali"}>
+          <h1>
+            {this.props.language == "0"
+              ? this.state.loader && this.state.texts[0].title1
+              : this.state.loader && this.state.texts[0].title_nep1}
 
-                this.setState({
-                    texts: text,
-                    loader: true
+            <span>
+              {" "}
+              {this.props.language == "0"
+                ? this.state.loader && this.state.texts[0].title2
+                : this.state.loader && this.state.texts[0].title_nep2}
+            </span>
+            <br />
+            {this.props.language == "0"
+              ? this.state.loader && this.state.texts[0].title3
+              : this.state.loader && this.state.texts[0].title_nep3}
+          </h1>
+          <p>
+            {this.props.language == "0"
+              ? this.state.loader && this.state.texts[0].description
+              : this.state.loader && this.state.texts[0].description_nep}
+          </p>
+        </div>
+        <div
+          className="app-icons-content"
+          style={{
+            marginLeft: "40vw",
+            marginTop: "110px",
+          }}
+        >
+          {/* <h4>{ this.props.language=='0' ? 'Download now' : 'डाउनलोड गर्नुहोस्' }</h4> */}
+          <div className="app-icon flex-start">
+            <figure>
+              <a
+                href="https://apps.apple.com/sk/app/open-space-nepal/id1534918535"
+                target="_blank"
+              >
+                <img src={require("../../img/appstore.png")} alt="ios" />
+              </a>
+            </figure>
+            <figure>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.iom.openspaces&hl=en&gl=US"
+                target="_blank"
+              >
+                <img src={require("../../img/googleplay.png")} alt="android" />
+              </a>
+            </figure>
+          </div>
+          <div
+            className="app-icon flex is-align-center"
+            style={{ marginLeft: "-3rem", gap: "1rem" }}
+          >
+            <a
+              href="https://apps.apple.com/sk/app/open-space-nepal/id1534918535"
+              target="_blank"
+            >
+              <img
+                src={require("../../img/logo1.png")}
+                alt="ios"
+                style={{ width: "11rem", backgroundColor: "white" }}
+              />
+            </a>
 
-                })
-            })
-
-    }
-    componentDidMount() {
-        this.getData();
-
-
-    }
-
-
-    render() {
-
-
-        return (
-
-            <div className="banner-content">
-
-                <div className={this.props.language == '0' ? "intro" : "intro-nepali"}>
-
-
-                    <h1>{this.props.language == '0' ? this.state.loader && this.state.texts[0].title1 : this.state.loader && this.state.texts[0].title_nep1}
-
-                        <span> {this.props.language == '0' ? this.state.loader && this.state.texts[0].title2 : this.state.loader && this.state.texts[0].title_nep2}</span>
-                        <br />
-                        {this.props.language == '0' ? this.state.loader && this.state.texts[0].title3 : this.state.loader && this.state.texts[0].title_nep3}
-                    </h1>
-                    <p>
-                        {this.props.language == '0' ? this.state.loader && this.state.texts[0].description : this.state.loader && this.state.texts[0].description_nep}</p>
-
-
-
-                </div>
-                <div className="app-icons-content" style={{
-                    marginLeft: '40vw',
-                    marginTop: '18px'
-                }}>
-
-                    {/* <h4>{ this.props.language=='0' ? 'Download now' : 'डाउनलोड गर्नुहोस्' }</h4> */}
-                    <div className="app-icon flex-start">
-                        <figure>
-                            <a href='https://apps.apple.com/sk/app/open-space-nepal/id1534918535' target='_blank'>
-                                <img src={require('../../img/appstore.png')} alt="ios" />
-                            </a>
-                        </figure>
-                        <figure>
-                            <a href='https://play.google.com/store/apps/details?id=com.iom.openspaces&hl=en&gl=US' target='_blank'>
-                                <img src={require('../../img/googleplay.png')} alt="android" />
-                            </a>
-                        </figure>
-                    </div>
-                    <div className="app-icon flex is-align-center" style={{ marginLeft: "-3rem", gap: "1rem" }}>
-
-                        <a href='https://apps.apple.com/sk/app/open-space-nepal/id1534918535' target='_blank'  >
-                            <img src={require('../../img/logo1.png')} alt="ios" style={{ width: '11rem', backgroundColor: "white" }} />
-                        </a>
-
-
-                        <a href='https://play.google.com/store/apps/details?id=com.iom.openspaces&hl=en&gl=US' target='_blank'>
-                            <img src={require('../../img/logo2.png')} alt="android" style={{ width: '11rem', backgroundColor: "white" }} />
-                        </a>
-
-                    </div>
-                </div>
-            </div >
-
-        )
-    }
+            <a
+              href="https://play.google.com/store/apps/details?id=com.iom.openspaces&hl=en&gl=US"
+              target="_blank"
+            >
+              <img
+                src={require("../../img/logo2.png")}
+                alt="android"
+                style={{ width: "11rem", backgroundColor: "white" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        language: state.language
-    }
-}
-
+  return {
+    language: state.language,
+  };
+};
 
 export default connect(mapStateToProps)(Banner);
